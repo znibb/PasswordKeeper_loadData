cbc45a16850ebe43181e727dff36052219375ac6
znibb <pontus.pson@gmail.com>
2017-01-02 18:44:54 +0100

    Moved 'maginc numbers' to named parameters

--- src/PasswordKeeper_loadData.ino
+++ src/PasswordKeeper_loadData.ino
@@ -3,27 +3,48 @@
 
 #define EEPROM_SIZE 1023  // atmega32u4 has 1 kB
 
+// Variables
 int memAddr = 0;
 
+// Parameters
+const byte uid_size = 7;        // Max length of uid array
+const byte password_size = 30;  // Max length of password char array
+
+// Struct defining uid/password pair
 struct tagData{
-  byte uid[7];
-  char password[30];
+  byte uid[uid_size];
+  char password[password_size];
 };
+
+// ###############################
+// ########## User data ##########
+// ###############################
 struct tagData tags[] = {
   {{0xFF, 0xFF, 0xFF, 0xFF},                    "test1\n"},
   {{0xFE, 0xFE, 0xFE, 0xFE},                    "test2\n"},
   {{0xFD, 0xFD, 0xFD, 0xFD, 0xFD, 0xFD, 0xFD},  "test3\n"},
   {{0xFC, 0xFC, 0xFC, 0xFC},                    "test4\n"}
 };
+// ###############################
+// ###############################
+// ###############################
+
+// Count how many tags were entered
 const byte nbrOfTags = sizeof(tags)/sizeof(tagData);
 
 void setup(){
+  // Initialize serial connecton
   Serial.begin(115200);
+
+  // Wait for serial connection
   while(!Serial){
     ;
   }
 
+  // Calculate size of the data the user wants to store
   int memUsage = sizeof(nbrOfTags) + sizeof(tags);
+
+  // Break if the required memory is larger than the available memory
   if(memUsage > EEPROM_SIZE){
     Serial.print("Size of information (");
     Serial.print(memUsage);
@@ -33,9 +54,11 @@ void setup(){
     return;
   }
 
+  // Store number of tags in first byte and increment memory index
   EEPROM.put(memAddr, nbrOfTags);
   memAddr += sizeof(nbrOfTags);
 
+  // Write data struct to memory
   EEPROM.put(memAddr, tags);
   Serial.print("Successfully wrote ");
   Serial.print(nbrOfTags);
@@ -45,5 +68,6 @@ void setup(){
 }
 
 void loop(){
+  // Infinite loop when done
   delay(10000);
 }
